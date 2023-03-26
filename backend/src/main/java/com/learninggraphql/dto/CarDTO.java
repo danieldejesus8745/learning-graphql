392585892ab331f397584b5bd58cf3e9f87082c4
Daniel de Jesus <danieldejesus8745@gmail.com>
2023-03-26 15:03:47 -0300

    creating entity Car, CarDTO and adding docker-compose.yml

---
+++ backend/src/main/java/com/learninggraphql/dto/CarDTO.java
@@ -0,0 +1,18 @@
+package com.learninggraphql.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CarDTO {
+
+    private Integer id;
+    private String name;
+    private String manufacturer;
+
+}
---
+++ backend/src/main/java/com/learninggraphql/entities/Car.java
@@ -0,0 +1,25 @@
+package com.learninggraphql.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String manufacturer;
+
+}
