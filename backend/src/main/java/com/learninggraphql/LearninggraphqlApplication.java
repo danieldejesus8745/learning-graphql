af408d053bb9f4dfbf3c2391025cd0eee45d3213
Daniel de Jesus <danieldejesus8745@gmail.com>
2023-03-26 14:44:24 -0300

    initiating the project

---
+++ backend/src/main/java/com/learninggraphql/LearninggraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.learninggraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearninggraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearninggraphqlApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/learninggraphql/LearninggraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learninggraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearninggraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
